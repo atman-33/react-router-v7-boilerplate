bab3ec34096dea39a551338095bd60427474942c
atman-33 <gpbjk0304@gmail.com>
2025-01-31 09:05:57 +0900

    feat: PoC用のトップページを追加

--- app/routes/_app._index/route.tsx
+++ app/routes/_app._index/route.tsx
@@ -17,19 +17,8 @@ export default function AppPage() {
       <Welcome />
       <div className="flex justify-center">
         <div className="flex flex-col gap-4">
-          <Link to="/poc/sample-csr" className="flex justify-center">
-            <Button className="w-80">Go to Sample CSR (Pokemon List)</Button>
-          </Link>
-          <Link to="/poc/sample-ssr" className="flex justify-center">
-            <Button className="w-80">Go to Sample SSR (Pokemon List)</Button>
-          </Link>
-          <Link to="/poc/sample-ssg" className="flex justify-center">
-            <Button className="w-80">Go to Sample SSG (About Me)</Button>
-          </Link>
-          <Link to="/poc/sample-ssr-csr" className="flex justify-center">
-            <Button className="w-80">
-              Go to Sample SSR & CSR (Pokemon List)
-            </Button>
+          <Link to="/poc" className="flex justify-center">
+            <Button className="w-80">Go to PoC</Button>
           </Link>
         </div>
       </div>
