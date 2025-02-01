d02066516de90eb56612b515a9d7f748ea1b9016
atman-33 <gpbjk0304@gmail.com>
2025-02-02 00:46:07 +0900

    feat: ローディングドットの表示見直し

--- app/components/shared/loading-dots/loading-dots.tsx
+++ app/components/shared/loading-dots/loading-dots.tsx
@@ -4,9 +4,9 @@ export const LoadingDots = () => {
       <div className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 transform">
         <div className="flex h-screen items-center justify-center space-x-2 dark:invert">
           <span className="sr-only">Loading...</span>
-          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400 [animation-delay:-0.3s]" />
-          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400 [animation-delay:-0.15s]" />
-          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400" />
+          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400 bg-opacity-50 [animation-delay:-0.3s]" />
+          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400 bg-opacity-50 [animation-delay:-0.15s]" />
+          <div className="h-8 w-8 animate-bounce rounded-full bg-blue-400 bg-opacity-50" />
         </div>
       </div>
     </div>
