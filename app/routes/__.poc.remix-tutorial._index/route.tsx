437159b4ddab8fea9126dc8c6c9d867080eadf27
atman-33 <gpbjk0304@gmail.com>
2025-02-01 22:42:15 +0900

    feat: トップページの表示部分を追加

--- app/routes/__.poc.remix-tutorial._index/route.tsx
+++ app/routes/__.poc.remix-tutorial._index/route.tsx
@@ -1,5 +1,75 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '~/components/shadcn/ui/card';
+
 const RemixTutorialTopPage = () => {
-  return <div>RemixTutorialHomePage</div>;
+  const pokemons = [
+    {
+      name: 'bulbasaur',
+      height: 7,
+      weight: 69,
+      base_experience: 64,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      },
+    },
+    {
+      name: 'charmander',
+      height: 6,
+      weight: 85,
+      base_experience: 62,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+      },
+    },
+    {
+      name: 'squirtle',
+      height: 5,
+      weight: 90,
+      base_experience: 63,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+      },
+    },
+  ];
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="font-bold text-2xl">Welcome to the Pokemon App</h1>
+      <p className="text-lg">Explore the world of Pokemon with PokeAPI</p>
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3">
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.name}>
+            <CardHeader>
+              <CardTitle>{pokemon.name}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+              <CardDescription>Height: {pokemon.height}</CardDescription>
+              <CardDescription>Weight: {pokemon.weight}</CardDescription>
+              <CardDescription>
+                Base Experience: {pokemon.base_experience}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <p className="mt-4">
+        Data sourced from{' '}
+        <a href="https://pokeapi.co/" className="text-blue-500 underline">
+          PokeAPI
+        </a>
+        .
+      </p>
+    </div>
+  );
 };
 
 export default RemixTutorialTopPage;
