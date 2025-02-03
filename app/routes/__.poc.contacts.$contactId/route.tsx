8f9248a892771337b667b66ae48e92f28385f66e
atman-33 <gpbjk0304@gmail.com>
2025-02-03 23:22:09 +0900

    refactor: Outletの位置リファクタリング

--- app/routes/__.poc.contacts.$contactId/route.tsx
+++ app/routes/__.poc.contacts.$contactId/route.tsx
@@ -24,68 +24,71 @@ const ContactPage = ({ loaderData }: Route.ComponentProps) => {
   }
 
   return (
-    <div className="flex gap-12">
-      <div>
-        <img
-          alt={`${contact.first} ${contact.last} avatar`}
-          key={contact.avatar}
-          src={contact.avatar}
-          className="w-60 rounded-3xl"
-        />
-      </div>
+    <>
+      <div className="flex gap-12">
+        <div>
+          <img
+            alt={`${contact.first} ${contact.last} avatar`}
+            key={contact.avatar}
+            src={contact.avatar}
+            className="w-60 rounded-3xl"
+          />
+        </div>
 
-      <div className="flex flex-col gap-6">
-        <h1 className="flex gap-4 font-extrabold text-3xl">
-          {contact.first || contact.last ? (
-            <>
-              {contact.first} {contact.last}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}
-          <Favorite contact={contact} />
-        </h1>
+        <div className="flex flex-col gap-6">
+          <h1 className="flex gap-4 font-extrabold text-3xl">
+            {contact.first || contact.last ? (
+              <>
+                {contact.first} {contact.last}
+              </>
+            ) : (
+              <i>No Name</i>
+            )}
+            <Favorite contact={contact} />
+          </h1>
 
-        {contact.twitter ? (
-          <p>
-            <a
-              href={`https://twitter.com/${contact.twitter}`}
-              className=" text-blue-600 text-xl"
-              target="_blank"
-              rel="noreferrer"
+          {contact.twitter ? (
+            <p>
+              <a
+                href={`https://twitter.com/${contact.twitter}`}
+                className=" text-blue-600 text-xl"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {contact.twitter}
+              </a>
+            </p>
+          ) : null}
+
+          {contact.notes ? <p>{contact.notes}</p> : null}
+
+          <div className="flex gap-4">
+            <Form action="edit">
+              <Button type="submit">Edit</Button>
+            </Form>
+
+            <Form
+              action="destroy"
+              method="post"
+              onSubmit={(event) => {
+                const response = confirm(
+                  'Please confirm you want to delete this record.',
+                );
+                if (!response) {
+                  event.preventDefault();
+                }
+              }}
             >
-              {contact.twitter}
-            </a>
-          </p>
-        ) : null}
-
-        {contact.notes ? <p>{contact.notes}</p> : null}
-
-        <div className="flex gap-4">
-          <Form action="edit">
-            <Button type="submit">Edit</Button>
-          </Form>
-
-          <Form
-            action="destroy"
-            method="post"
-            onSubmit={(event) => {
-              const response = confirm(
-                'Please confirm you want to delete this record.',
-              );
-              if (!response) {
-                event.preventDefault();
-              }
-            }}
-          >
-            <Button variant="destructive" type="submit">
-              Delete
-            </Button>
-          </Form>
+              <Button variant="destructive" type="submit">
+                Delete
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
+      {/* モーダル表示用のOutlet */}
       <Outlet />
-    </div>
+    </>
   );
 };
 
