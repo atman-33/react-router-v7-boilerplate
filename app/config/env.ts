128e43091da3e7301d2a5e066efa97d43fde2822
atman-33 <gpbjk0304@gmail.com>
2025-02-24 19:55:20 +0900

    feat: Google認証用の環境変数を追加

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,3 +1,6 @@
 export const env = {
   SESSION_SECRET: process.env.SESSION_SECRET as string,
+  CLIENT_URL: process.env.CLIENT_URL as string,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
 };
